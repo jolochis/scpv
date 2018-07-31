05ee3a62c3822a17596cc88f7cc035798f76eb73
jolochis <jolochis@gmail.com>
2018-07-31 15:49:59 -0500

    Update condb.php
    parámetros de heroku

--- includes/condb.php
+++ includes/condb.php
@@ -3,7 +3,8 @@
 //$condb = pg_conne	ct("host=localhost dbname=gdlMap_ user=postgres password=mapas") or die("Error en la conexion");
 
 //$condb2 = pg_connect("host=localhost port=5432 dbname=scpv_tags user=postgres password=user"); //conectamos a la
-$condb = pg_connect("host=localhost port=5432 dbname=scpv user=postgres password=postgres"); //conectamos a la 
+//local$condb = pg_connect("host=localhost port=5432 dbname=scpv user=postgres password=postgres"); //conectamos a la 
+$condb = pg_connect("host=ec2-54-204-23-228.compute-1.amazonaws.com port=5432 dbname=ds6ceq7df0qqm user=ruzvacvovwxefq password=6c17463c8074344e6d02cb7770477bcf658c3c78dd8c3bcaee419c24ccdd7666"); //heroku
 if(!$condb){
 	die("Error de conexion DB: ".pg_last_error($condb));
 }
