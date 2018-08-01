575c872d9a06a667b3c5c91afa30f828b355ff30
jolochis <jolochis@gmail.com>
2018-08-01 12:16:31 -0500

    Update condb.php

--- includes/condb.php
+++ includes/condb.php
@@ -1,7 +1,8 @@
 <?php
 
 //local$condb = pg_connect("host=localhost port=5432 dbname=scpv user=postgres password=postgres"); //conectamos a la 
-$condb = pg_connect("host=ec2-54-204-23-228.compute-1.amazonaws.com port=5432 dbname=ds6ceq7df0qqm user=ruzvacvovwxefq password=6c17463c8074344e6d02cb7770477bcf658c3c78dd8c3bcaee419c24ccdd7666") or die("Error en la conexion"); //heroku
+$condb = pg_connect("host=ec2-54-204-23-228.compute-1.amazonaws.com
+ port=5432 dbname=ds6ceq7df0qqm user=ruzvacvovwxefq password=6c17463c8074344e6d02cb7770477bcf658c3c78dd8c3bcaee419c24ccdd7666") or die("Error en la conexion"); //heroku
 if(!$condb){
 	die("Error de conexion DB: ".pg_last_error($condb));
 }
